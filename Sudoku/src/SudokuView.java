68c100fd359f49f90142c9e0728f1e6834553901
andreascarlsson <andreascarlsson@andreas-mbp>
2018-12-10 14:43:44 +0100

    .

--- Sudoku/src/SudokuView.java
+++ Sudoku/src/SudokuView.java
@@ -1,8 +1,16 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-public class SudokuView {
+public class SudokuView extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
 
 	public static void main(String[] args) {
-		
+		// TODO Auto-generated method stub
 
 	}
 
