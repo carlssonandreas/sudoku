ccfb34c2ac7675455ec3370d8676db1906156dda
andreascarlsson <andreascarlsson@andreas-mbp>
2018-12-10 13:46:47 +0100

    .

--- Sudoku/src/Sudoku.java
+++ Sudoku/src/Sudoku.java
@@ -1,36 +1,45 @@
 
-
-
 public class Sudoku {
-	
-private int[][] board;
-	
-	
-public Sudoku(){
-	board = new int[9][9];
-	
-}
 
-public void insertNumber(Object a, int i, int j){
+	private int[][] board;
 
-	if(a instanceof Integer){
-		
-	}else{
-		
+	public Sudoku() {
+		board = new int[9][9];
+
 	}
-	
-}
 
-public int getNumber(int i, int j){
-	return board[i][j];
-}
+	public void insertNumber(Object a, int i, int j) {
 
-public void clear(){
-	
-}
+		if (a instanceof Integer) {
 
-public boolean solve(int i, int j){
-	return false;
-}
+		} else {
+
+		}
+
+	}
+
+	public int getNumber(int i, int j) {
+		return board[i][j];
+	}
+
+	public void clear() {
+		board = new int[9][9];
+
+	}
+
+	public boolean solve(int i, int j) {
+		if (board[i][j] == 0) {
+
+		}
+
+		return false;
+	}
+
+	private boolean helpSolve(int i, int j) {
+		if (i < 4 && j < 4) {
+
+		}
+		return false;
+	}
 
 }
