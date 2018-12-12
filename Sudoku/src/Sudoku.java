bf456693b91c4d4a26e7125862eef76fe95163d1
andreascarlsson <andreascarlsson@10.1.24.204>
2018-12-12 14:10:11 +0100

    Documentation done.

--- Sudoku/src/Sudoku.java
+++ Sudoku/src/Sudoku.java
@@ -1,22 +1,20 @@
 
 public class Sudoku {
 
-
 	private int[][] board;
 
+	public Sudoku(){
+		board = new int[9][9];
 	
+	}
 	
-public Sudoku(){
-	board = new int[9][9];
-	
-}
-/**
- * Kontrollerar om a är en integer om inte=> throw exception
- * Om a integer kontrollera om mellan 1-9
- * Om det är det sätt in på plats i,j i sudokut
- * Om inte throw Exception
- */
-public void insertNumber(Object a, int i, int j) {
+	/**
+	 * Control if a is of the type Integer. if yes,control if the number is between 1 and 9.
+	 * If yes, add a at position i,j in the sudoku.
+	 * @throws IllegalArgumentException if it was not possible to add a.
+	 * @param a object that is controlled to be number and than are added to the sudoku.
+	 */
+	public void insertNumber(Object a, int i, int j) {
 
 		if(a instanceof Integer){
 			int number= ((Integer) a).intValue();
@@ -34,16 +32,31 @@ public void insertNumber(Object a, int i, int j) {
 			
 		}
 	}
-
+	/**
+	 * Returns the number at position i,j in board.
+	 * @param i the row in board.
+	 * @param j the column in board.
+	 * @return the number on place i,j in board.
+	 */
 	public int getNumber(int i, int j) {
 		return board[i][j];
 	}
 
+	/**
+	 * Makes the board empty.
+	 */
 	public void clear() {
 		board = new int[9][9];
 
 	}
 
+	/**
+	 * Controls if the sudoku is solveble by trying to find a solution 
+	 * with the initial numbers in the board.
+	 * @param i the row in board from where the solution shall start.
+	 * @param j the column in board from where the solution shall start.
+	 * @return true if the sudoku is solveble and false if not.
+	 */
 	public boolean solve(int i, int j) {
 		return helpSolve(i,j);
 	}
@@ -94,13 +107,13 @@ public void insertNumber(Object a, int i, int j) {
 	}
 
 	
-/**
- * Denna metod kontrollerar först att värdet inte finns i raden, sen kolumnen och sen rutan
- * @param number
- * @param row
- * @param col
- * @return
- */
+	/**
+	 * Controls if the number at position i,j is allowed according to rules of sudoku.
+	 * @param number the number that is controlled.
+	 * @param row the row in board where the number belongs to.
+	 * @param col the comumn in board where the number belongs to.
+	 * @return true if the number is allowed and false if not.
+	 */
 	private boolean isAllowed(int number, int row, int col){
 		//Kontrollera värdet med raden
 		for(int n=0;n<9;n++){
